6c660e9839a087bc88a0125a6800a16ce5768d40
Gokul2728 <gokulat8@gmail.com>
2024-03-28 10:31:44 +0530

    commit

--- lib/cool_validator.dart
+++ lib/cool_validator.dart
@@ -1,7 +1,8 @@
 library cool_validator;
 
 /// A Calculator.
-// class Calculator {
-//   /// Returns [value] plus 1.
-//   int addOne(int value) => value + 1;
-// }
+/// fjghhdgfjn
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
